c0d2db6c4d1a114e6ac5513ae683d058e897eb03
Mark <MarkKleinfelder@gmail.com>
2017-09-18 11:47:54 -0600

    creating api. seeded. audioAPI/audios

--- server.js
+++ server.js
@@ -10,20 +10,32 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db = require('./models')
 
+app.get('/api', function api_index (req, res){
+  res.json({
+    message: "Welcome to Mark Kleinfelder's Project 4",
+    documentation_url: "",
+    base_url: "",
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes available endpoints"}
+    ]
+  });
+});
 
 
-app.get('/api/audioFiles', function programs_index(req, res){
-  db.Program.find({},function(error, audioFiles){
-    console.log(programs);
-    res.json(programs);
+
+
+app.get('/api/audios', function audios_index(req, res){
+  db.Audio.find({},function(error, audios){
+    console.log(audios);
+    res.json(audios);
  });
 });
 
 
-app.get('/api/audioFiles/:id', function(req,res){
-  db.Program.findOne({ _id:req.params.id
-	}, function(err,audioFiles){
-	  res.json(program);
+app.get('/api/audios/:id', function(req,res){
+  db.Audio.findOne({ _id:req.params.id
+	}, function(err,audios){
+	  res.json(audios);
   })
 })
 
